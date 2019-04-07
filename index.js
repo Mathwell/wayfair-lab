332c9d8769b9f1f383b81b80b86277614323b47f
Olena Ageyeva <eageeva@hotmail.com>
2019-04-07 20:59:12 +0000

    bottom method added

--- index.js
+++ index.js
@@ -92,13 +92,21 @@ class LeaderBoard {
   };
 
   top(num_players){
-    if (num_players<0 || !Number.isInteger(num_players)) { return "invalid number"}
     let topList=Object.keys(this.players).map(key=>
       [Number(key), this.players[key].average()]).sort((a,b)=>b[1]-a[1]).map(element=>element[0]).slice(0,num_players)
 
     return topList
 
   };
+
+  bottom(num_players){
+    let topList=Object.keys(this.players).map(key=>
+      [Number(key), this.players[key].average()]).sort((a,b)=>a[1]-b[1]).map(element=>element[0]).slice(0,num_players)
+
+    return topList
+
+  };
+
 
   reset(player_id){
     if (this.players[player_id]) {
@@ -112,6 +120,16 @@ class LeaderBoard {
 
 // Test code here
 
+function array_equals(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
 
 var leader_board = new LeaderBoard()
 leader_board.add_score(1, 50)
@@ -124,8 +142,23 @@ console.log('Add score should return the average. test with 2 scores')
 console.log(leader_board.add_score(3, 85) == 87.5)
 console.log('Top 3 [' + leader_board.top(3) + '] should equal [3, 2, 1]:')
 console.log(array_equals(leader_board.top(3), [3, 2, 1]))
+
+console.log('Top 3 [' + leader_board.bottom(3) + '] should equal [1, 2, 3]:')
+console.log(array_equals(leader_board.bottom(3), [1, 2, 3]))
+
+
+
 console.log('Top 2 [' + leader_board.top(2) + '] should equal [3, 2]:')
 console.log(array_equals(leader_board.top(2), [3, 2]))
+
+console.log('Top 2 [' + leader_board.bottom(2) + '] should equal [1, 2]:')
+console.log(array_equals(leader_board.bottom(2), [1, 2]))
+
+
+
 leader_board.reset(3)
 console.log('After reset top 3 [' + leader_board.top(3) + '] should equal [2, 1, 3]')
 console.log(array_equals(leader_board.top(3), [2, 1, 3]))
+
+console.log('After reset bottom 3 [' + leader_board.bottom(3) + '] should equal [3, 1, 2]')
+console.log(array_equals(leader_board.bottom(3), [3, 1, 2]))
